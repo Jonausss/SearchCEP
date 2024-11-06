d921e4206406313362f8471d1fa48937e23438d7
Jonas <jp.marcilio2@gmail.com>
2024-11-06 17:28:26 -0300

    0.2

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import './Styles.css'
 
 function App() {
 
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
+
+  function HandleSearch() {
+    alert("Valor do input: " + input);
+  }
 
   return(
     <div className="container">
       <h1 className="title">Search CEP</h1>
       <div className="containerInput">
-        <input type="text" placeholder="Type the CEP"/>
-        <button type="button" className="buttonSearch"><FiSearch size={25} color="black"/></button>
+        <input type="text" placeholder="Type the CEP" value={input} onChange={(e) => setInput(e.target.value)}/>
+        <button type="button" className="buttonSearch" onClick={HandleSearch}><FiSearch size={25} color="black"/></button>
       </div>
       <main className="main">
         <h2>091284098</h2>
